15c7e848aff37d489eeb2afd4fdbecac26d08b29
jacksonsophat <37005847+jacksonsophat@users.noreply.github.com>
2023-02-02 22:40:49 -0600

    Local News add Austin

--- projects/tests.py
+++ projects/tests.py
@@ -1,14 +1,56 @@
 import feedparser
+from dateutil import parser
 
 
 
+# data = []
+# NBC = feedparser.parse("https://www.click2houston.com/arc/outboundfeeds/rss/category/news/local/?outputType=xml&size=10")
+# NBC_Entries = NBC.entries  
+# for item in NBC_Entries:
+#     title = item['title']  
+#     print(title)
 
-data = []
-KHOU = feedparser.parse("https://www.khou.com/feeds/syndication/rss/news/local")
-KHOU_Entries = KHOU.entries  
-for item in KHOU_Entries:
-    title = item['title']  
-    print(title)
+# print(NBC_Entries)
 
-# print(KHOU_Entries)
 
+# def sort_dict_by_value(d, reverse = False):
+#   return dict(sorted(d.items(), key = lambda x: x[1], reverse = reverse))
+
+# print("Original dictionary elements:")
+# colors = {'Red': 1, 'Green': 3, 'Black': 5, 'White': 2, 'Pink': 4}
+
+# print(colors)
+# print("\nSort (ascending) the said dictionary elements by value:")
+# print(sort_dict_by_value(colors))
+# print("\nSort (descending) the said dictionary elements by value:")
+# print(sort_dict_by_value(colors, True))
+
+
+publishedTime = [
+    {
+     "published": parser.parse("Fri, 27 Jan 2023 00:06:07 +0000"),
+    },
+    {
+    "published": parser.parse("Thu, 26 Jan 2023 14:39:44 GMT"),
+    },
+    {
+    "published": parser.parse("Thu, 26 Jan 2023 20:14:39 GMT"),
+    },
+]
+
+sorted_dict = sorted(publishedTime["published"], key=lambda item: item[1])
+
+print(sorted_dict)
+
+# sorted_dict = sorted([(value, key)
+# for (key, value) in publishedTime.items()])
+
+
+# sorted_dict = sorted(publishedTime["published"])
+
+# print(sorted_dict)
+
+# x = parser.parse("Fri, 27 Jan 2023 00:06:07 +0000")
+
+# x =[parser.parse("Fri, 27 Jan 2023 00:06:07 +0000"),parser.parse("Thu, 26 Jan 2023 14:39:44 GMT"),parser.parse("Thu, 26 Jan 2023 20:14:39 GMT") ]
+# print(sorted(x))
--- projects/views.py
+++ projects/views.py
@@ -11,12 +11,24 @@ import time
 @api_view(['GET'])
 def newsOutlet(request):
     data=[]
-    KHOU = feedparser.parse("https://www.khou.com/feeds/syndication/rss/news/local")
-    KHOU_Entries = KHOU.entries  
-    for item in KHOU_Entries:
-        title = item['title']  
-        print(title)
-        data.append(title)
+    #City: Austin
+    # AustinChronicle
+    AustinChronicle = feedparser.parse("https://www.austinchronicle.com/gyrobase/rss/daily-news.xml")
+    AustinChronicle_Entries = AustinChronicle.entries 
+    for item in AustinChronicle_Entries:
+        try:
+            image = item['links'][1]['href']
+        except:
+            image = False
+        news = {
+            'station':'Austin Chronicle',
+            'title': item['title'],
+            'summary': item['summary'],
+            'link': item['link'],
+            'published': item['published'],
+            'image': image
+            }
+        data.append(news)
 
     return Response(data)
 
@@ -40,28 +52,129 @@ def newsOutlet(request):
 
 @api_view(['GET'])
 def localNews(request):
+    select_city=''
     data=[]
+    # select_city = 'houston'
+    if request.query_params.get('select_city'):
+        select_city = request.query_params.get('select_city')
+        if (select_city == 'houston'):
+            # City = Houston
+            # ABC Feed
+            KTRK = feedparser.parse("https://abc13.com/feed/")
+            KTRK_Entries = KTRK.entries    
+            # KTRK_logo = '/images/projects/local-news/ktrk.png'
+            for item in KTRK_Entries:
+                time = item['published']
+                try:
+                    image = item['media_thumbnail'][0]['url']
+                except:
+                    image = False
+                news = {
+                'station':'ABC13',
+                'title': item['title'],
+                'summary': item['summary'],
+                'link': item['link'],
+                'published': time,
+                'image': image
+                }
+                data.append(news)
 
-    # ABC Feed
-    ABC13 = feedparser.parse("https://abc13.com/feed/")
-    ABC13_Entries = ABC13.entries    
-    for item in ABC13_Entries:
-        time = parser.parse(item['published'])
-        time.strftime("%a-%m-%d-%Y %I:%M %p")
-        try:
-            image = item['media_thumbnail'][0]['url']
-        except:
-            image = False
+            # KHOU
+            KHOU = feedparser.parse("https://www.khou.com/feeds/syndication/rss/news/local")
+            KHOU_Entries = KHOU.entries  
+            # KHOU_logo = '/images/projects/local-news/khou.png'
+            for item in KHOU_Entries:
+                link = item['links'][0]['href']
+                try:
+                    image = item['links'][1]['href']
+                except:
+                    image = False
+                
+                news = {
+                    'station':'KHOU',
+                    'title': item['title'],
+                    'summary': item['summary'],
+                    'link': link,
+                    'published': item['published'],
+                    'image': image
+                    }
+                data.append(news)
+            
+            # NBC
+            KPRC = feedparser.parse("https://www.click2houston.com/arc/outboundfeeds/rss/category/news/local/?outputType=xml&size=10")
+            KPRC_Entries = KPRC.entries 
+            # KPRC_logo ='/images/projects/local-news/kprc.png'
+            for item in KPRC_Entries:
+                # image = item['media_content'][0]['url']
+                try:
+                    image = item['media_content'][0]['url']
+                except:
+                    image = False
+                news = {
+                    'station':'KPRC',
+                    'title': item['title'],
+                    'summary': item['summary'],
+                    'link': item['link'],
+                    'published': item['published'],
+                    'image': image
+                    }
+                data.append(news)   
+        elif (select_city == 'austin'):
+            #City: Austin
+            # NBC
+            KXAN = feedparser.parse("https://www.kxan.com/feed/")
+            KXAN_Entries = KXAN.entries 
+            for item in KXAN_Entries[:10]:
+                # link = 
 
-        news = {
-            'station':'ABC 13',
-        'title': item['title'],
-        'summary': item['summary'],
-        'link': item['link'],
-        'published': time,
-        'image': image
-        }
-        data.append(news)
-    # 
+                try:
+                    image = item['links'][1]['href']
+                except:
+                    image = False
+                news = {
+                    'station':'KXAN',
+                    'title': item['title'],
+                    'summary': item['summary'],
+                    'link': item['link'],
+                    'published': item['published'],
+                    'image': image
+                    }
+                data.append(news)
+            
+            # KVUE
+            KVUE = feedparser.parse("https://www.kvue.com/feeds/syndication/rss/news/local")
+            KVUE_Entries = KVUE.entries 
+            for item in KVUE_Entries[:10]:
+                try:
+                    image = item['links'][1]['href']
+                except:
+                    image = False
+                news = {
+                    'station':'KVUE',
+                    'title': item['title'],
+                    'summary': item['summary'],
+                    'link': item['link'],
+                    'published': item['published'],
+                    'image': image
+                    }
+                data.append(news)
+            
+            # AustinChronicle
+            AustinChronicle = feedparser.parse("https://www.austinchronicle.com/gyrobase/rss/daily-news.xml")
+            AustinChronicle_Entries = AustinChronicle.entries 
+            for item in AustinChronicle_Entries[:10]:
+                try:
+                    image = item['links'][1]['href']
+                except:
+                    image = False
+                news = {
+                    'station':'Austin Chronicle',
+                    'title': item['title'],
+                    'summary': item['summary'],
+                    'link': item['link'],
+                    'published': item['published'],
+                    'image': image
+                    }
+                data.append(news)
     return Response(data)
 
